85d13a84b9dcf4c347b3fa1f9b188daee8c486fa
Thantry <thasr03@ca.com>
2019-02-26 07:39:08 +0530

    updating how it works zip code

---
+++ ui.apps/src/main/content/jcr_root/apps/nexterahome/components/content/howitworks/clientlib-banner/js/howitworksvalidation.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+
+ $('#howitworkzip').click(function(event){
+
+     var zipcode = $('#ziphowitworks').val();
+
+
+       $.ajax({
+
+            type: 'GET', 
+			url:'/bin/zipcodevalidation',
+            async: false,
+            data: 'zipcode='+ zipcode,
+            success: function(responseData){
+			console.log(JSON.stringify(responseData.validZipcode));
+
+            if(responseData.validZipcode){
+
+            	if(responseData.service=='nextEra'){
+         			 	 event.preventDefault();
+                        window.location = "/content/nexterahome/en/homestrcture/serviceplans-illinois.html";
+        		}else if(responseData.service=='FPL'){
+
+            			event.preventDefault();
+            			window.location = "/content/nexterahome/en/homestrcture/thank-you-fpl.html";
+
+        		}else if(responseData.service=='texas'){
+                        event.preventDefault();
+                        window.location = "/content/nexterahome/en/homestrcture/serviceplans-texas.html";
+
+                    }else{
+                        event.preventDefault();
+                        window.location = "/content/nexterahome/en/homestrcture/thank-you.html";
+
+                    }
+
+
+            }else{
+
+                alert(responseData.service);
+            }
+
+
+            }
+
+        });
+
+ });
+});
