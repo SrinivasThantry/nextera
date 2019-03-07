9f73eee2fd45bf11e69ff680fa575a6bd15d1ecb
Thantry <thasr03@ca.com>
2019-03-07 14:58:06 +0530

    updated all validation including new pages

--- ui.apps/src/main/content/jcr_root/apps/nexterahome/components/content/headernavigation/clientlib-banner/js/headerzipvalidation.js
+++ ui.apps/src/main/content/jcr_root/apps/nexterahome/components/content/headernavigation/clientlib-banner/js/headerzipvalidation.js
@@ -1,12 +1,67 @@
 
 $(document).ready(function(){
 
+    var pagename = "";
+
+
     console.log(":::::::::enter:");
+
+    $("#closeid").click(function(){
+ 		   $("#zipcodeGate").removeAttr("style");
+           $("#zipcodeGate").attr("style", "display: none;");
+         	$("#zipcodeGate").attr("aria-hidden", "true");
+           $("#zipcodeGate").removeClass('show');
+
+     });
+
+
+ 	$("#closeidbtn").click(function(){
+ 		   $("#zipcodeGate").removeAttr("style");
+           $("#zipcodeGate").attr("style", "display: none;");
+         	$("#zipcodeGate").attr("aria-hidden", "true");
+           $("#zipcodeGate").removeClass('show');
+
+     });
+
+	$('a[href="#load_links"]').click(function(event){
+	   $('#zipCode').val("");
+       var text = event.currentTarget.innerText;
+       console.dir("test"+event);
+		pagename = text;
+       if(text === 'FAQ' || text === 'SERVICE PLANS'){
+			$('#zipcodeGate').addClass('show');
+            $("#zipcodeGate").attr("style", "display: block; padding-right: 17px;");
+
+       }else{
+
+           $("#zipcodeGate").removeAttr("style");
+           $("#zipcodeGate").attr("style", "display: none;");
+           $("#zipcodeGate").removeClass('show');
+           if(text === 'HOME'){
+
+               event.preventDefault();
+               window.location = "/content/nexterahome/en/homestrcture/home.html";
+
+           }else if(text === 'OUR PHILOSOPHY'){
+ 			   event.preventDefault();
+               window.location = "/content/nexterahome/en/homestrcture/ourphilosophy.html";
+
+
+           }
+
+       }
+
+
+
+   })
+
 
  $('#formzipcodesubmit').click(function(event){
 
      var zipcode = $('#zipCode').val();
 
+     	if(zipcode.length == 0)
+            return;
 
 
 
@@ -21,17 +76,31 @@ $(document).ready(function(){
 
             if(responseData.validZipcode){
 
-            	if(responseData.service=='nextEra'){
+           var redirectpath = "";
+
+
+
+             if(responseData.service=='nextEra'){
+ 					if(pagename === 'SERVICE PLANS')
+           				 redirectpath = '/content/nexterahome/en/homestrcture/serviceplans/serviceplans-illinois.html';
+          		 	if(pagename === 'FAQ')
+          				 redirectpath = '/content/nexterahome/en/homestrcture/faq/faq-illinois.html';
          			 	 event.preventDefault();
-                        window.location = "/content/nexterahome/en/homestrcture/serviceplans-illinois.html";
+                        window.location = redirectpath;
         		}else if(responseData.service=='FPL'){
 
             			event.preventDefault();
             			window.location = "/content/nexterahome/en/homestrcture/thank-you-fpl.html";
 
         		}else if(responseData.service=='texas'){
-                        event.preventDefault();
-                        window.location = "/content/nexterahome/en/homestrcture/serviceplans-texas.html";
+
+                    if(pagename === 'SERVICE PLANS')
+           				 redirectpath = '/content/nexterahome/en/homestrcture/serviceplans/serviceplans-texas.html';
+          		 	if(pagename === 'FAQ')
+          				 redirectpath = '/content/nexterahome/en/homestrcture/faq/faq-texas.html';
+         			 	 event.preventDefault();
+                        window.location = redirectpath;
+
 
                     }else{
                         event.preventDefault();
